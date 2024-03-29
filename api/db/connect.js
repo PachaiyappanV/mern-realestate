bae9f035ba6617a92c056bbea873e4668ae77672
PachaiyappanV <pachaiyappan394@gmail.com>
2024-03-29 18:20:04 +0530

    connect to the database

---
+++ api/db/connect.js
@@ -0,0 +1,9 @@
+const { connect } = require("mongoose");
+
+const connectDB = (uri) => {
+  return connect(uri);
+};
+
+module.exports = {
+  connectDB,
+};
--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const { connectDB } = require("./db/connect");
 const app = express();
 
-app.listen(3000, () => console.log("server is running on port 3000"));
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("Database connected successfully");
+    app.listen(port, () => {
+      console.log(`server is up and running on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
